d3ba8680548c404bbc62da9e0120a33c2bb7ad0f
sheffey <57262511+SheffeyG@users.noreply.github.com>
2024-05-04 03:43:11 +0800

    CMake with cleaner workspace

--- build.sh
+++ build.sh
@@ -1,2 +1,12 @@
-cc -o main main.c -I raylib-5.0_linux_amd64/include -L raylib-5.0_linux_amd64/lib -l:libraylib.a -lm
-./main
+#!/bin/bash
+
+# cc -o main main.c -I raylib-5.0_linux_amd64/include -L raylib-5.0_linux_amd64/lib -l:libraylib.a -lm
+
+if [ ! -d "./build" ]; then
+    mkdir build
+fi
+
+cd ./build
+
+cmake ..
+make 
